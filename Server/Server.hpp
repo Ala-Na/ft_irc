e541a53c7ac16404735320dc0ab0aa5b3b7efa82
Nadege ALAVOINE <anadege@e3r1p5.clusters.42paris.fr>
2022-04-05 11:38:13 +0200

    starting .cpp

--- Server/Server.hpp
+++ Server/Server.hpp
@@ -1,5 +1,23 @@
+#ifndef USER_HPP
+
 #include <vector>
 #include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <cstring>
+#include <errno.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+#include <netdb.h>
+#include <vector>
+#include <poll.h>
+#include <fcntl.h>
+#include <signal.h>
+
 // TODO modify following path
 #include "User.hpp"
 #include "Channel.hpp"
@@ -10,14 +28,17 @@ class Server {
 		std::vector<User *>		users;
 		std::vector<Channel *>	channels; // NOTE must keep track of channels modes
 
-		std::string				name; // NOTE max 63 characters
+		std::string				name = "In Real unControl - An ft_irc server"; // NOTE max 63 characters
 		std::string				password;
 		const char				*port;
+		bool					running = true;
 
 	public :
 		Server (); // TODO maybe set in private ?
-		Server (std::string name, std::string password, int port); // TODO need port ? password ?
+		Server (std::string password, int port);
 		~Server ();
 
 
 };
+
+#endif
