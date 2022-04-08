e31cca07b1b38e3ef722ad62481d1f942e5c6324
Nadege ALAVOINE <anadege@e3r1p5.clusters.42paris.fr>
2022-04-08 16:36:04 +0200

    It's fun (no)

--- Numerics.cpp
+++ Numerics.cpp
@@ -11,30 +11,29 @@ void	sendNumeric(int fd, std::string msg) {
 	write(fd, msg.c_str(), msg.size());
 }
 
-/* 001 */ std::string	RplWelcome (std::string server, std::string nick, std::string network, std::string user, std::string host)
-{ return (" :Welcome to the " + network + " Network, " + nick + "!" + user + "@" + host); }
+/* 001 */ std::string	RplWelcome (std::string server, std::string nick, std::string network, std::string user, std::string host) { return (" :Welcome to the " + network + " Network, " + nick + "!" + user + "@" + host); }
 /* 002 */ std::string	RplYourHost (std::string server, std::string version) { return (" :Your host is " + server + ", running version " + version);}
 /* 003 */ std::string	RplCreated (std::string creation) { return (" :This server was created " + creation); }
-/* 004 */ std::string	RplMyInfo (std::string server, std::string version, std::string u_modes, std::string c_modes, std::string supp_c_modes)
-{ return (" " + server + " " + version + " " + u_modes + " " + c_modes + " " + supp_c_modes); }
+/* 004 */ std::string	RplMyInfo (std::string server, std::string version, std::string u_modes, std::string c_modes, std::string supp_c_modes) { return (" " + server + " " + version + " " + u_modes + " " + c_modes + " " + supp_c_modes); }
 /* 005 */ std::string	RplBounce (std::string server, std::string port) { return (" : Try server " + server + ", port " + port); }
 /* 020 - Not in RFC, but used in irssi */ std::string	RplProcessConnection () { return (" :Please wait while we process your connection."); }
-/* 301 */ std::string	RplAway (std::string dest_nick, std::string away_msg) {
-	return (" " + dest_nick + " :" + away_msg); }
-/* 302 */
-// WARNING Need to generate reply before
-std::string	RplUserhost (std::string replies) {
-	return (" :" + replies);
-	// there can be multiple replies, which should be in format :
-	// If operator without away message : nickname*=-hostname
-	// If operator with away message : nickname*=+hostname
-	// If non-operator without away message : nickname=-hostname
-	// If non-operator with away message : nickname=+hostname
-}
-/* 305 */ std::string RplUnaway () { return (" :You are no longer marked as being away"); }
-/* 306 */ std::string RplNowAway () { return (" : You have been marked as being away"); }
-/* 311 */ std::string RplWhoIsUser (std::string w_nick, std::string w_user, std::string w_host, std::string w_real) { return (" " + w_nick + " " + w_user + " " + w_host + " * :" + w_real); }
-/* 312 */ std::string RplWhoIsServer (std::string server, std::string infos) { return (" " + server + " :" + infos); };
+/* 301 */ std::string	RplAway (std::string dest_nick, std::string away_msg) { return (" " + dest_nick + " :" + away_msg); }
+/* 302 */ std::string	RplUserhost (std::string replies) { return (" :" + replies); }
+/* 305 */ std::string	RplUnaway () { return (" :You are no longer marked as being away"); }
+/* 306 */ std::string	RplNowAway () { return (" : You have been marked as being away"); }
+/* 311 */ std::string	RplWhoIsUser (std::string w_nick, std::string w_user, std::string w_host, std::string w_real) { return (" " + w_nick + " " + w_user + " " + w_host + " * :" + w_real); }
+/* 312 */ std::string	RplWhoIsServer (std::string server, std::string infos) { return (" " + server + " :" + infos); };
+/* 313 */ std::string	RplWhoIsOperator (std::string o_nick) { return (" " + o_nick + " :is an IRC operator"); }
+// 317 - No need
+/* 318 */ std::string	RplEndOfWhoIs () { return (" :End of WHOIS list"); }
+// 319 - No need
+/* 322 */ std::string	RplList (std::string channel, std::string visible, std::string topic) { return (" " + channel + " " + visible + " :" + topic); }
+/* 323 */ std::string	RplListEnd () { return (" :End of LIST"); }
+/* 324 */
+
+// Examples :
+// 442 :openirc.snt.utwente.nl 442 lolilol #Cimitero :You're not on that channel
+// 474 :openirc.snt.utwente.nl 474 lolilol #Cimitero :Cannot join channel (+b)
 
 
 // TODO check that server_name is in (...) for function where it's needed
@@ -91,8 +90,22 @@ void	irc::numericReply(int num_nb, int fd, std::string server, \
 			break;
 		case 311:
 			msg += RplWhoIsUser(s_params[0], s_params[1], s_params[2], s_params[3]);
+			break;
 		case 312:
 			msg += RplWhoIsServer(server, s_params[0]);
+			break;
+		case 313:
+			msg += RplWhoIsOperator(s_params[0]);
+			break;
+		case 318:
+			msg += RplEndOfWhoIs();
+			break;
+		case 322:
+			msg += RplList(s_params[0], s_params[1], s_params[2]);
+			break;
+		case 323:
+			msg += RplListEnd();
+			break;
 		default :
 			// TODO set error
 			break;
@@ -112,6 +125,10 @@ int main() {
 	irc::numericReply(302, 1, "server", "nick", 1, "user=+/-host");
 	irc::numericReply(305, 1, "server", "nick", 0);
 	irc::numericReply(306, 1, "server", "nick", 0);
-	irc::numericReply(311, 1, "server", "nick", 4, "nick", "user", "host", "real_name");
+	irc::numericReply(311, 1, "server", "nick", 4, "nick2", "user", "host", "real_name");
 	irc::numericReply(312, 1, "server", "nick", 1, "Infos about server");
+	irc::numericReply(313, 1, "server", "nick", 1, "nick2");
+	irc::numericReply(318, 1, "server", "nick", 0);
+	irc::numericReply(322, 1, "server", "nick", 3, "channel", "visible", "topic");
+	irc::numericReply(323, 1, "server", "nick", 0);
 }
