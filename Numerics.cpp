7eff4b4e2c09706403d88c0754edf6824278ae4f
Nadege ALAVOINE <anadege@e3r1p5.clusters.42paris.fr>
2022-04-08 15:47:10 +0200

    I already hate this

--- Numerics.cpp
+++ Numerics.cpp
@@ -11,41 +11,17 @@ void	sendNumeric(int fd, std::string msg) {
 	write(fd, msg.c_str(), msg.size());
 }
 
-// 001
-std::string	RplWelcome (std::string server, std::string nick, std::string network, \
-	std::string user, std::string host)
-{
-	return (" :Welcome to the " + network + " Network, " + nick + "!" + user + \
-		"@" + host);
-}
-// 002
-std::string	RplYourHost (std::string server, std::string version) {
-	return (" :Your host is " + server + ", running version " + version);
-}
-// 003
-std::string	RplCreated (std::string creation) {
-	return (" :This server was created " + creation);
-}
-// 004
-std::string	RplMyInfo (std::string server, std::string version, \
-	std::string u_modes, std::string c_modes, std::string supp_c_modes)
-{
-	return (" " + server + " " + version + " " + u_modes + " " +\
-		c_modes + " " + supp_c_modes);
-}
-// 005
-std::string	RplBounce (std::string server, std::string port) {
-	return (" : Try server " + server + ", port " + port);
-}
-// 020 - Not in RFC, but used in irssi
-std::string	RplProcessConnection () {
-	return (" :Please wait while we process your connection.");
-}
-// 301
-std::string	RplAway (std::string nick, std::string dest_nick, std::string away_msg) {
-	return (nick + " " + dest_nick + " :" + away_msg);
-}
-// 302
+/* 001 */ std::string	RplWelcome (std::string server, std::string nick, std::string network, std::string user, std::string host)
+{ return (" :Welcome to the " + network + " Network, " + nick + "!" + user + "@" + host); }
+/* 002 */ std::string	RplYourHost (std::string server, std::string version) { return (" :Your host is " + server + ", running version " + version);}
+/* 003 */ std::string	RplCreated (std::string creation) { return (" :This server was created " + creation); }
+/* 004 */ std::string	RplMyInfo (std::string server, std::string version, std::string u_modes, std::string c_modes, std::string supp_c_modes)
+{ return (" " + server + " " + version + " " + u_modes + " " + c_modes + " " + supp_c_modes); }
+/* 005 */ std::string	RplBounce (std::string server, std::string port) { return (" : Try server " + server + ", port " + port); }
+/* 020 - Not in RFC, but used in irssi */ std::string	RplProcessConnection () { return (" :Please wait while we process your connection."); }
+/* 301 */ std::string	RplAway (std::string dest_nick, std::string away_msg) {
+	return (" " + dest_nick + " :" + away_msg); }
+/* 302 */
 // WARNING Need to generate reply before
 std::string	RplUserhost (std::string replies) {
 	return (" :" + replies);
@@ -55,6 +31,10 @@ std::string	RplUserhost (std::string replies) {
 	// If non-operator without away message : nickname=-hostname
 	// If non-operator with away message : nickname=+hostname
 }
+/* 305 */ std::string RplUnaway () { return (" :You are no longer marked as being away"); }
+/* 306 */ std::string RplNowAway () { return (" : You have been marked as being away"); }
+/* 311 */ std::string RplWhoIsUser (std::string w_nick, std::string w_user, std::string w_host, std::string w_real) { return (" " + w_nick + " " + w_user + " " + w_host + " * :" + w_real); }
+/* 312 */ std::string RplWhoIsServer (std::string server, std::string infos) { return (" " + server + " :" + infos); };
 
 
 // TODO check that server_name is in (...) for function where it's needed
@@ -98,11 +78,21 @@ void	irc::numericReply(int num_nb, int fd, std::string server, \
 			msg += RplProcessConnection();
 			break;
 		case 301:
-			msg += RplAway(nick, s_params[0], s_params[1]);
+			msg += RplAway(s_params[0], s_params[1]);
+			break;
 		case 302:
 			msg += RplUserhost(s_params[0]);
 			break;
+		case 305:
+			msg += RplUnaway();
 			break;
+		case 306:
+			msg += RplNowAway();
+			break;
+		case 311:
+			msg += RplWhoIsUser(s_params[0], s_params[1], s_params[2], s_params[3]);
+		case 312:
+			msg += RplWhoIsServer(server, s_params[0]);
 		default :
 			// TODO set error
 			break;
@@ -118,5 +108,10 @@ int main() {
 	irc::numericReply(2, 1, "server", "nick", 1, "version");
 	irc::numericReply(3, 1, "server", "nick", 1, "date");
 	irc::numericReply(4, 1, "server", "nick", 4, "version", "mode user", "mode channel", "more");
+	irc::numericReply(301, 1, "server", "nick", 2, "from_dest", "Is away");
 	irc::numericReply(302, 1, "server", "nick", 1, "user=+/-host");
+	irc::numericReply(305, 1, "server", "nick", 0);
+	irc::numericReply(306, 1, "server", "nick", 0);
+	irc::numericReply(311, 1, "server", "nick", 4, "nick", "user", "host", "real_name");
+	irc::numericReply(312, 1, "server", "nick", 1, "Infos about server");
 }
