350f5c3c8fd99aba5977b1b7360f5661714bef3b
anadege <anadege@student.42.fr>
2022-04-25 15:46:12 +0200

    slight changes

--- Command.cpp
+++ Command.cpp
@@ -198,7 +198,10 @@ void	Command::intPrivMsg() {
 			}
 			return;
 		}
-		this->param.erase(0, params[0].size() + 2);
+		this->param.erase(0, params[0].size() + 1);
+		if (param[0] == ':') {
+			this->param.erase(0, 1);
+		}
 		this->user->privmsgToUser(dest, this->param);
 	} else {
 		Channel*	chan = this->server.getChannelByName(params[0]);
@@ -208,7 +211,10 @@ void	Command::intPrivMsg() {
 			}
 			return;
 		}
-		this->param.erase(0, params[0].size() + 2);
+		this->param.erase(0, params[0].size() + 1);
+		if (param[0] == ':') {
+			this->param.erase(0, 1);
+		}
 		this->user->privmsgToChannel(chan, this->param);
 	}
 }
@@ -229,14 +235,20 @@ void	Command::intNotice() {
 		if (dest == NULL) {
 			return;
 		}
-		this->param.erase(0, params[0].size() + 2);
+		this->param.erase(0, params[0].size() + 1);
+		if (param[0] == ':') {
+			this->param.erase(0, 1);
+		}
 		this->user->noticeToUser(dest, this->param);
 	} else {
 		Channel*	chan = this->server.getChannelByName(params[0]);
 		if (chan == NULL) {
 			return;
 		}
-		this->param.erase(0, params[0].size() + 2);
+		this->param.erase(0, params[0].size() + 1);
+		if (param[0] == ':') {
+			this->param.erase(0, 1);
+		}
 		this->user->noticeToChannel(chan, this->param);
 	}
 }
